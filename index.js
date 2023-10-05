7fa558c8e46bea15902bcaa3a638c0a389405053
Stein-B06 <Steinb06.kodehode@gmail.com>
2023-10-05 11:18:24 +0200

    added JS

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ const imageData = [
   { src: "turkey.jpg", description: "Turkey sound", sound: "turkey.mp3" },
   { src: "horse.jpg", description: "Horse sound", sound: "horse.mp3" },
 ];
+// Function to create and add image cards to the page
+function createAndAddImageCard() {
+  const imageGrid = document.getElementById(ìmage - grid);
+
+  imageData.forEach((data)) => {
+    const imageCard = document.createElement(`div`)
+ imageCard.classList.add(`image-card`)
+
+ const image = document.createElement(`img`)
+ image.src =data.src;
+ image.alt = data.description;
+
+ const description = document.createElement(`p`);
+description.textContent = data.description;
+
+ //added click event so that image plays sound and shakes
+imageCard.addEventListener(`click`, () => {
+  playSound(data.sound)
+  shakeImage(image);
+}
+)
+imageCard.appendChild(image);
+imageCard.appendChild(description);
+imageGrid.appendChild(imageCard);
+
+  }
+}
