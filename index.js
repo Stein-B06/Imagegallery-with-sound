0e73911c9b15dd8f6f9e5118287c18f219dc8c88
Stein-B06 <Steinb06.kodehode@gmail.com>
2023-10-05 11:01:48 +0200

    Added sounds and images to JS

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-const imageCards = document.querySelectorAll(".image-card");
-const sounds = document.querySelectorAll("audio");
-
-imageCards.forEach((card, index) => {
-  const image = card.querySelector("img");
-  card.addEventListener("click", () => {
-    playSound(index);
-    shakeImage(image, sounds[index]);
-  });
-
-  // Add key event listeners (1-6)
-  window.addEventListener("keydown", (e) => {
-    if (e.key >= "1" && e.key <= "6") {
-      const keyIndex = parseInt(e.key) - 1;
-      playSound(keyIndex);
-      shakeImage(imageCards[keyIndex].querySelector("img"), sounds[keyIndex]);
-    }
-  });
-});
-
-function playSound(index) {
-  if (index >= 0 && index < sounds.length) {
-    sounds[index].currentTime = 0;
-    sounds[index].play();
-  }
-}
-
-function shakeImage(image, sound) {
-  image.style.animation = "shake 0.5s";
-  sound.addEventListener("ended", () => {
-    image.style.animation = "none";
-  });
-}
+//Added the data for the images and the sounds.
+const imageData = [
+  { src: "cow.jpg", description: "Cow sound", sound: "cow.mp3" },
+  { src: "pig.jpg", description: "Pig sound", sound: "pig.mp3" },
+  { src: "rooster.jpg", description: "Rooster sound", sound: "rooster.mp3" },
+  { src: "sheep.jpg", description: "Sheep sound", sound: "sheep.mp3" },
+  { src: "turkey.jpg", description: "Turkey sound", sound: "turkey.mp3" },
+  { src: "horse.jpg", description: "Horse sound", sound: "horse.mp3" },
+];
